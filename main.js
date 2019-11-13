5814640c5838dd08aaf9f4798ac783c53b656673
Michele De Rosa <info@michelederosa.it>
2019-11-13 09:33:55 +0100

    Layout Completed

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document ).ready(function() {
+
+    // cambia icona audio/invia
+    $("#message").click(function () {
+      $(".audio-record").hide();
+      $(".send-message").show();
+    });
+});

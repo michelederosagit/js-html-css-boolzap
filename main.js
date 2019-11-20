c916fe2b5cd15bee8054c3ba80a833aaada9331c
Michele De Rosa <info@michelederosa.it>
2019-11-20 16:30:29 +0100

    Completed Milestone 3

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document ).ready(function() {
+    selectChat();
 
     // Cambia icona audio/invia quando viene cliccato l'input
     $("#message").click(function () {
@@ -25,7 +26,7 @@ $(document ).ready(function() {
     });
     // Funzione per rimuove i messaggi
     function deleteMsg(){
-      $(".chat").on('click','.pUsr .dropdown',function(){
+      $("#chat").on('click','.pUsr .dropdown',function(){
         $(this).siblings().remove();
         $(this).remove();
       });
@@ -49,6 +50,26 @@ $(document ).ready(function() {
       });
     });
 
+    // Funzione per cambio conversazione
+    function selectChat () {
+      $(".chat-list-box .chat-list").click(function() {
+        // Rimuovo la classe open agli altri elementi e l'aggiungo a quello cliccato
+        $(".chat-list-box .chat-list").removeClass("open");
+        $(this).addClass("open");
+        // Recupero l'attributo data-contact dalla conversazione selezionata
+        var dataContact = $(this).attr("data-contact");
+        // Recupero il nome del contatto
+        var nameContact = $(this).find(".name").text();
+        // Inserisco il nome nell'head della conversazione
+        $(".head-chat .user-information-box .user-information span.name").text(nameContact);
+        // Cerco la conversazione con lo stesso attributo e la rendo visibile mentre nascondo le altre
+        $(".container #chat").addClass("hide");
+        $(".container #chat[data-contact='" + nameContact + "']").removeClass("hide");
+        console.log(dataContact);
+        
+      });
+    };// Fine Funzione SelectChat
+
     // Creo funzione invio messaggio utente e bot
     function sendMsg(){
       // Salvo il messaggio scritto dall'utente
@@ -67,15 +88,13 @@ $(document ).ready(function() {
       // var elementMsgSent = elementMessage.text(sentMessage);
 
       // Inserisco il messaggio tra il mio div clonato "elementMessage" e il tag <p>
-      var outputUsr = elementUsrMessage.html("<p>" + sentUsrMessage + "</p>" + "<i class='fas fa-angle-down icona-menu'>" + "</i>" + "<i class='fas fa-angle-up icona-menu-up'>" + "</i>" + "<div class='dropdown'>" + "<a href='#'>" + "<span class='text-delete'>" + "Elimina" + "</span>" + "</a>");
+      var outputUsr = elementUsrMessage.html("<p>" + sentUsrMessage + "</p>" + "<i class='fas fa-angle-down icona-menu'>" + "</i>" + "<div class='dropdown'>" + "<a href='#'>" + "<span class='text-delete'>" + "Elimina" + "</span>" + "</a>");
       
       // Inserisco il DIV + Messaggio utente dopo una classe messa all'interno dell'html
-      $(".chat").append(outputUsr);
+      $(".container #chat:not(.hide)").append(outputUsr);
       // ripuliamo il contenuto dell'input, per UX
       $("#message").val("");
       // Fine funzione utente
-
-
 
       // Creo funzione per chatbot
       function botMessageText(){
@@ -87,22 +106,28 @@ $(document ).ready(function() {
         var outputBot = elementBotMessage.html("<p>" + "Ciao, io sono un bot" + "</p>");
         
         // Inserisco il DIV + Messaggio utente dopo una classe messa all'interno dell'html
-        $(".chat").append(outputBot);
+        $("#chat:not(.hide)").append(outputBot);
       };
       setTimeout(botMessageText,1000); // Fine funzione bot
 
-      // Mostro / nascondo l'icona in base al mouse
-      $(".chat .pUsr p").on({
-        mouseenter: function() {
-          $(".icona-menu").show();
-        },
-        mouseleave: function (){
-          $(".icona-menu").hide();
-        }
+      // Hover (mouse enter e leave) sul messaggio per far comparire la freccia delle opzioni
+      $('#chat .pUsr').on('mouseenter', function() {
+        $(this).find('.icona-menu').addClass('show');
       });
-      // Se l'icona viene cliccata allora mostro menù a tendina
-      $(".icona-menu").click(function() {
-        $(".dropdown").toggle();
+      $('#chat .pUsr').on('mouseleave', function() {
+        $(this).find('.icona-menu').removeClass('show');
+      });
+
+
+      // Al click sulla freccia delle opzioni appare l'opzione per cancellare il messaggio
+      $('.icona-menu').on('click', function() {
+        $(this).siblings('.dropdown').addClass('show');
+      });
+      $('.icona-menu').on('dblclick', function() {
+        $(this).siblings('.dropdown').removeClass('show');
+      });
+      // al click su delete messaggio lo cancello
+      $('.text-delete').on('click', function(){
         deleteMsg();
       });
     }; // Fine Funzione send
